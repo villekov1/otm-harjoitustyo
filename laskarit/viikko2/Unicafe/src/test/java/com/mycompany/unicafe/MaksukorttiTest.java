1633eeac66128dab92693face4e072e307923615
Ville Kovanen <ville.kovanen@helsinki.fi>
2018-03-20 13:25:08 +0200

    Toinen osa eli maksukortti ja kassapääte -tehtävä

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -11,10 +11,47 @@ public class MaksukorttiTest {
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
+        //Huom. kortilla aluksi vain kymmenen senttiä
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoAlussaOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaArvoa(){
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenToimiiJosRahaaEiRiittavasti(){
+        kortti.otaRahaa(100);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenToimiiJosRahaaRiittavasti(){
+        kortti.lataaRahaa(200);
+        kortti.otaRahaa(105);
+        //Rahaa 1 euro ja 5 senttiä
+        
+        assertEquals("saldo: 1.5", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenPalauttaaTruen(){
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void rahanOttaminenPalauttaaFalsen(){
+        assertEquals(false, kortti.otaRahaa(20));
     }
 }
